9b00a70515f44265c96e737ef97e74d55b428eb9
Joel Lonbeck <joel@neutrinographics.com>
2018-05-24 16:04:47 -0700

    updated linux builds to use custom release path

--- scripts/run-build-linux.sh
+++ scripts/run-build-linux.sh
@@ -7,11 +7,13 @@ cd code-base/
 node --version
 npm --version
 
+HASH=$(git rev-parse HEAD | cut -c1-7)
+echo $HASH > ../commit.txt
+VERSION=$(node -p -e "require('./package.json').version")
+echo $VERSION > ../version.txt
+
 npm install
 npm install -g gulp@4.0.0
 npm run load-apps
 gulp build --linux || exit 1;
-gulp release --linux || exit 1;
-
-# copy into output
-cp -a release/. ../build/
+gulp release-linux --out=../build/linux/translationCore-linux-x64-$VERSION-$HASH.zip || exit 1;
