2fcdfa7bb2013bbe11743abe9fdb48344f7f595d
Joel Lonbeck <joel@neutrinographics.com>
2018-05-26 00:35:42 -0700

    removed redundant dep installs

--- scripts/run-build.sh
+++ scripts/run-build.sh
@@ -7,16 +7,8 @@ set -xe
 
 cd code-base/
 
-node --version
-npm --version
-
 HASH=$(git rev-parse HEAD | cut -c1-7)
-# echo $HASH > ../build_meta/commit.txt
 VERSION=$(node -p -e "require('./package.json').version")
-# echo $VERSION > ../build_meta/version.txt
-
-npm install
-npm install -g gulp@4.0.0
-npm run load-apps
+# npm install -g gulp@4.0.0
 
 HASH=$HASH VERSION=$VERSION ../concourse-tasks/scripts/run-build-$PLATFORM.sh
