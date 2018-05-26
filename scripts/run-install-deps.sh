fefa40d1a21fef0251f5143ac06da6af160c9d30
Joel Lonbeck <joel@neutrinographics.com>
2018-05-25 23:48:52 -0700

    added task to install dependencies

---
+++ scripts/run-install-deps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -xe
+
+# copy input to output
+./concourse-tasks/scripts/copy-dir.sh code-base code-with-dependencies
+
+cd code-with-dependencies/
+
+node --version
+npm --version
+
+npm ci
+npm run load-apps
