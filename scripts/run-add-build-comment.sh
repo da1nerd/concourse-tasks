6a53db2224b6563c04288862b2913e4af22ef93c
Joel Lonbeck <joel@neutrinographics.com>
2018-05-25 21:22:00 -0700

    fixes

--- scripts/run-add-build-comment.sh
+++ scripts/run-add-build-comment.sh
@@ -6,6 +6,11 @@ if [ -f build_meta/notification.txt ]; then
   cat build_meta/notification.txt
 fi
 
+# copy previous values
+cp meta/comment.md build_meta/comment.md
+cp meta/notification.txt build_meta/notification.txt
+
+# append new values
 echo $text >> build_meta/comment.md
 echo $text >> build_meta/notification.txt
 
