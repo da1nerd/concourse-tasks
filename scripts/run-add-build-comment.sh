65d71df3277ec4f035cea6a9d03c1e76e3dc63d9
Joel Lonbeck <joel@neutrinographics.com>
2018-05-25 21:13:51 -0700

    more test

--- scripts/run-add-build-comment.sh
+++ scripts/run-add-build-comment.sh
@@ -2,7 +2,9 @@
 
 set -xe
 
-cat build_meta/notification.txt
+if [ -f build_meta/notification.txt ]; then
+  cat build_meta/notification.txt
+fi
 
 echo $text >> build_meta/comment.md
 echo $text >> build_meta/notification.txt
