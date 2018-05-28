7e0388aeb907d3c96a536f604ad9b9487dd03674
Joel Lonbeck <joel@neutrinographics.com>
2018-05-28 14:17:43 -0700

    debugging output for windows builds

--- scripts/run-build-linux.sh
+++ scripts/run-build-linux.sh
@@ -9,5 +9,3 @@ FILE=translationCore-linux-x64-$VERSION-$HASH.zip
 
 echo "[Linux build $HASH (v$VERSION)](https://s3-us-west-2.amazonaws.com/tc-builds.door43.org/$FILE) is ready for download." >> ../build_meta/comment.md
 echo "<https://s3-us-west-2.amazonaws.com/tc-builds.door43.org/$FILE|Linux build $HASH (v$VERSION)> is ready for download." >> ../build_meta/notification.txt
-
-cat ../build_meta/notification.txt
--- scripts/run-build.sh
+++ scripts/run-build.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-# copy input to output
+# copy input metadata to output
 ./concourse-tasks/scripts/copy-dir.sh input_meta build_meta
 
 cd code-base/
@@ -10,4 +10,5 @@ cd code-base/
 HASH=$(git rev-parse HEAD | cut -c1-7)
 VERSION=$(node -p -e "require('./package.json').version")
 
+# execute the appropriate platform builder
 HASH=$HASH VERSION=$VERSION ../concourse-tasks/scripts/run-build-$PLATFORM.sh
