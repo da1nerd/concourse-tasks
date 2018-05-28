d227398d75544faec8ebbc1a1c1001b2f09cda29
Joel Lonbeck <joel@neutrinographics.com>
2018-05-28 15:55:53 -0700

    more dbugging

--- scripts/run-build-windows.sh
+++ scripts/run-build-windows.sh
@@ -6,7 +6,8 @@ FILE64=translationCore-win-x64-$VERSION-$HASH.setup.exe
 FILE32=translationCore-win-x32-$VERSION-$HASH.setup.exe
 
 ./node_modules/.bin/gulp build --win || exit 1;
-ls -la out
+ls -la .
+ls -la ../
 ./node_modules/.bin/gulp release-win64 --out=../build/windows/x64/$FILE64 || exit 1;
 ./node_modules/.bin/gulp release-win32 --out=../build/windows/x32/$FILE32 || exit 1;
 
