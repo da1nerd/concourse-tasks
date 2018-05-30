fa99d8b7f737962f101bd6434c1bfdb0211fa0fc
Joel Lonbeck <joel@neutrinographics.com>
2018-05-29 17:14:58 -0700

    added arch to windows build ntoes

--- scripts/run-build-windows.sh
+++ scripts/run-build-windows.sh
@@ -12,8 +12,8 @@ ls -la ../
 ./node_modules/.bin/gulp release-win32 --out=../build/windows/x32/$FILE32 || exit 1;
 
 # x64
-echo "[Windows build $HASH (v$VERSION)](https://s3-us-west-2.amazonaws.com/tc-builds.door43.org/$FILE64) is ready for download." >> ../build_meta/comment.md
+echo "[Windows x64 build $HASH (v$VERSION)](https://s3-us-west-2.amazonaws.com/tc-builds.door43.org/$FILE64) is ready for download." >> ../build_meta/comment.md
 echo "<https://s3-us-west-2.amazonaws.com/tc-builds.door43.org/$FILE64|Windows x64 build $HASH (v$VERSION)> is ready for download." >> ../build_meta/notification.txt
 # x32
-echo "[Windows build $HASH (v$VERSION)](https://s3-us-west-2.amazonaws.com/tc-builds.door43.org/$FILE32) is ready for download." >> ../build_meta/comment.md
+echo "[Windows x32 build $HASH (v$VERSION)](https://s3-us-west-2.amazonaws.com/tc-builds.door43.org/$FILE32) is ready for download." >> ../build_meta/comment.md
 echo "<https://s3-us-west-2.amazonaws.com/tc-builds.door43.org/$FILE32|Windows x32 build $HASH (v$VERSION)> is ready for download." >> ../build_meta/notification.txt
