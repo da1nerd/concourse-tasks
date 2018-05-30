d635626e1f39120c8b55bd07a42b7a22999d9a9f
Joel Lonbeck <joel@neutrinographics.com>
2018-05-30 11:48:33 -0700

    updated build logs

--- scripts/run-build-macos.sh
+++ scripts/run-build-macos.sh
@@ -7,5 +7,5 @@ FILE=translationCore-macos-x64-$VERSION-$HASH.dmg
 ./node_modules/.bin/gulp build --osx || exit 1;
 ./node_modules/.bin/gulp release-macos --out=../build/macos/$FILE || exit 1;
 
-echo "[macOS build $HASH (v$VERSION)](https://s3-us-west-2.amazonaws.com/tc-builds.door43.org/$FILE) is ready for download." >> ../build_meta/comment.md
+echo "[macOS build $VERSION ($HASH)](https://s3-us-west-2.amazonaws.com/tc-builds.door43.org/$FILE) is ready for download." >> ../build_meta/comment.md
 echo "<https://s3-us-west-2.amazonaws.com/tc-builds.door43.org/$FILE|macOS build $HASH (v$VERSION)> is ready for download." >> ../build_meta/notification.txt
