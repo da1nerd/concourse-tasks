1a9fd4aa1f967359403b19f03fcc75c6604e0e8a
Joel Lonbeck <joel@neutrinographics.com>
2018-06-15 14:54:54 -0700

    added s3 resource that uploads folder structures

--- scripts/run-build-linux.sh
+++ scripts/run-build-linux.sh
@@ -5,7 +5,7 @@ set -xe
 FILE=translationCore-linux-x64-$VERSION-$HASH.zip
 
 ./node_modules/.bin/gulp build --linux || exit 1;
-./node_modules/.bin/gulp release-linux --out=../build/linux/$FILE || exit 1;
+./node_modules/.bin/gulp release-linux --out=../build/$FILE || exit 1;
 
 echo "[Linux build $VERSION ($HASH)](https://s3-us-west-2.amazonaws.com/tc-builds.door43.org/$FILE) is ready for download." >> ../build_meta/comment.md
 echo "<https://s3-us-west-2.amazonaws.com/tc-builds.door43.org/$FILE|Linux build $HASH (v$VERSION)> is ready for download." >> ../build_meta/notification.txt
