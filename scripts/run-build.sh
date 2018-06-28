7f648479327161accd4d044e5fe5da345bb9edd3
Joel Lonbeck <joel@neutrinographics.com>
2018-06-27 17:14:16 -0700

    added helpdesk email to .env

--- scripts/run-build.sh
+++ scripts/run-build.sh
@@ -12,6 +12,7 @@ VERSION=$(node -p -e "require('./package.json').version")
 
 # TODO: it would be nicer to decrypt .env.enc instead
 echo "TC_HELP_DESK_TOKEN=$TC_HELP_DESK_TOKEN" >> .env
+echo "TC_HELP_DESK_EMAIL=$TC_HELP_DESK_EMAIL" >> .env
 echo "BUILD=$HASH" >> .env
 
 # execute the appropriate platform builder
